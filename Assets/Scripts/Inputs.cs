cfbb29cd45e06e6008355fd7ea93debdaee2ae68
Fernando Diago <fernandodl2435@gmail.com>
2018-01-17 19:41:11 +0100

    -
    -

---
+++ Assets/Scripts/Inputs.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inputs : MonoBehaviour
+{
+    float AxisX;
+
+	void Start ()
+    {
+		
+	}
+
+	void Update ()
+    {
+        AxisX = Input.GetAxis("horizontal");
+	}
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,15 +5,114 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 
-
+    PlayerState currentPlayerState;
+    enum PlayerState
+    {
+        Idle,
+        Move,
+        Spit,
+        Drink,
+        Dead,
+        Freeze,
+    }
 
 	void Start ()
     {
-		
+        IdleState();
 	}
 
 	void Update ()
     {
-		
-	}
+        switch(currentPlayerState)
+        {
+            case PlayerState.Idle:
+                Idle();
+                break;
+            case PlayerState.Move:
+                Move();
+                break;
+            case PlayerState.Spit:
+                Spit();
+                break;
+            case PlayerState.Drink:
+                Drink();
+                break;
+            case PlayerState.Dead:
+                Dead();
+                break;
+            case PlayerState.Freeze:
+                Freeze();
+                break;
+            default:
+                break;
+        }
+    }
+
+    #region UPDATE METHODS
+
+    void Idle()
+    {
+
+    }
+
+    void Move()
+    {
+        
+    }
+
+    void Spit()
+    {
+
+    }
+
+    void Drink()
+    {
+
+    }
+
+    void Dead()
+    {
+
+    }
+
+    void Freeze()
+    {
+
+    }
+
+    #endregion
+
+    #region STATE METHODS
+
+    void IdleState()
+    {
+        currentPlayerState = PlayerState.Idle;
+    }
+
+    void MoveState()
+    {
+        currentPlayerState = PlayerState.Move;
+    }
+
+    void SpitState()
+    {
+        currentPlayerState = PlayerState.Spit;
+    }
+
+    void DrinkState()
+    {
+        currentPlayerState = PlayerState.Drink;
+    }
+
+    void DeadState()
+    {
+        currentPlayerState = PlayerState.Dead;
+    }
+
+    void FreezeState()
+    {
+        currentPlayerState = PlayerState.Freeze;
+    }
+
+    #endregion
 }
