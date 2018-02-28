101b4583b709228f5534932888091ac26d8bb889
Fernando Diago <fernandodl2435@gmail.com>
2018-02-28 18:54:14 +0100

    Snail movement
    Snail movement in floor and wall

--- Assets/Scripts/Inputs.cs
+++ Assets/Scripts/Inputs.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class Inputs : MonoBehaviour
 {
-    float AxisX;
-
-	void Start ()
-    {
-		
-	}
+    bool pressingUp;
+    bool pressingLeft;
+    bool pressingDown;
+    bool pressingRight;
 
 	void Update ()
     {
-        AxisX = Input.GetAxis("horizontal");
-	}
+        if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            pressingUp = true;
+        }
+        else pressingUp = false;
+
+        if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            pressingLeft = true;
+        }
+        else pressingLeft = false;
+
+        if(Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            pressingDown = true;
+        }
+        else pressingDown = false;
+
+        if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            pressingRight = true;
+        }
+        else pressingRight = false;
+    }
+
+    public bool PressingUp { get { return pressingUp; } }
+    public bool PressingLeft { get { return pressingLeft; } }
+    public bool PressingDown { get { return pressingDown; } }
+    public bool PressingRight { get { return pressingRight; } }
+
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,34 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [Header("Game elements")]
+    Inputs inputs;
+
+    [Header("Box properties")]
+    public Vector2 topBoxPos;
+    public Vector2 topBoxSize;
+
+    public Vector2 leftBoxPos;
+    public Vector2 leftBoxSize;
+
+    public Vector2 bottomBoxPos;
+    public Vector2 bottomBoxSize;
+
+    public Vector2 rightBoxPos;
+    public Vector2 rightBoxSize;
+
+    [Header("Filter")]
+    public ContactFilter2D filter;
+    public int maxColliders = 1;
+
+    [Header("Player properties")]
+    float speed = 0.5f;
+
+    public bool topWalled;
+    public bool leftWalled;
+    public bool rightWalled;
+    public bool bottomWalled;
+    public bool snailingInWall;
 
     PlayerState currentPlayerState;
     enum PlayerState
@@ -18,10 +46,19 @@ public class Player : MonoBehaviour
 
 	void Start ()
     {
+        inputs = GameObject.FindGameObjectWithTag("Input").GetComponent<Inputs>();
         IdleState();
 	}
 
-	void Update ()
+    void FixedUpdate()
+    {
+        ResetState();
+        LeftWallDetection();
+        BottomWallDetection();
+        RightWallDetection();
+    }
+
+    void Update ()
     {
         switch(currentPlayerState)
         {
@@ -52,12 +89,67 @@ public class Player : MonoBehaviour
 
     void Idle()
     {
-
+        if(inputs.PressingUp || inputs.PressingLeft || inputs.PressingDown || inputs.PressingRight)
+        {
+            MoveState();
+        }
     }
 
     void Move()
     {
-        
+        if(!snailingInWall)
+        {
+            if(inputs.PressingLeft && !leftWalled)
+            {
+                Vector3 provisionalPos = this.transform.position;
+
+                provisionalPos.x -= speed * Time.deltaTime;
+
+                this.gameObject.transform.position = provisionalPos;
+            }
+            else if(inputs.PressingLeft && leftWalled)
+            {
+                snailingInWall = true;
+            }
+
+            if(inputs.PressingRight && !rightWalled)
+            {
+                Vector3 provisionalPos = this.transform.position;
+
+                provisionalPos.x += speed * Time.deltaTime;
+
+                this.gameObject.transform.position = provisionalPos;
+            }
+            else if(inputs.PressingRight && rightWalled)
+            {
+                snailingInWall = true;
+            }
+        }
+
+        if(snailingInWall)
+        {
+            if(inputs.PressingUp && !topWalled)
+            {
+                Vector3 provisionalPos = this.transform.position;
+
+                provisionalPos.y += speed * Time.deltaTime;
+
+                this.gameObject.transform.position = provisionalPos;
+            }
+
+            if(inputs.PressingDown && !bottomWalled)
+            {
+                Vector3 provisionalPos = this.transform.position;
+
+                provisionalPos.y -= speed * Time.deltaTime;
+
+                this.gameObject.transform.position = provisionalPos;
+            }
+            else if(inputs.PressingDown && bottomWalled)
+            {
+                snailingInWall = false;
+            }
+        }
     }
 
     void Spit()
@@ -78,6 +170,58 @@ public class Player : MonoBehaviour
     void Freeze()
     {
 
+    }
+
+    #endregion
+
+    #region DETECTION METHODS
+
+    void ResetState()
+    {
+        topWalled = false;
+        leftWalled = false;
+        bottomWalled = false;
+        rightWalled = false;
+    }
+
+    void TopWallDetection()
+    {
+        Vector3 pos = this.transform.position + (Vector3)topBoxPos;
+        Collider2D[] results = new Collider2D[maxColliders];
+
+        int numColliders = Physics2D.OverlapBox(pos, topBoxSize, 0, filter, results);
+
+        if(numColliders > 0) topWalled = true;
+    }
+
+    void LeftWallDetection()
+    {
+        Vector3 pos = this.transform.position + (Vector3)leftBoxPos;
+        Collider2D[] results = new Collider2D[maxColliders];
+
+        int numColliders = Physics2D.OverlapBox(pos, leftBoxSize, 0, filter, results);
+
+        if(numColliders > 0) leftWalled = true;
+    }
+
+    void BottomWallDetection()
+    {
+        Vector3 pos = this.transform.position + (Vector3)bottomBoxPos;
+        Collider2D[] results = new Collider2D[maxColliders];
+
+        int numColliders = Physics2D.OverlapBox(pos, bottomBoxSize, 0, filter, results);
+
+        if(numColliders > 0) bottomWalled = true;
+    }
+
+    void RightWallDetection()
+    {
+        Vector3 pos = this.transform.position + (Vector3)rightBoxPos;
+        Collider2D[] results = new Collider2D[maxColliders];
+
+        int numColliders = Physics2D.OverlapBox(pos, rightBoxSize, 0, filter, results);
+
+        if(numColliders > 0) rightWalled = true;
     }
 
     #endregion
@@ -115,4 +259,24 @@ public class Player : MonoBehaviour
     }
 
     #endregion
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+        Vector3 pos1 = this.transform.position + (Vector3)topBoxPos;
+        Gizmos.DrawWireCube(pos1, topBoxSize);
+
+        Gizmos.color = Color.red;
+        Vector3 pos = this.transform.position + (Vector3)leftBoxPos;
+        Gizmos.DrawWireCube(pos, leftBoxSize);
+
+        Gizmos.color = Color.yellow;
+        Vector3 pos4 = this.transform.position + (Vector3)bottomBoxPos;
+        Gizmos.DrawWireCube(pos4, bottomBoxSize);
+
+        Gizmos.color = Color.green;
+        Vector3 pos2 = this.transform.position + (Vector3)rightBoxPos;
+        Gizmos.DrawWireCube(pos2, rightBoxSize);
+        
+    }
 }
