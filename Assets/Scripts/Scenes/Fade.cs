d9e943af38f8e35e9a4771fa48e0d429e8f83080
Fernando <fernandodl2435@gmail.com>
2018-04-16 16:55:02 +0200

    scenes

---
+++ Assets/Scripts/Scenes/Fade.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fade : MonoBehaviour
+{
+    bool transparent;
+    bool fadeIn;
+    bool fadeOut;
+
+    float counter = 2;
+
+    void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+		if(fadeIn)
+        {
+            if(counter > 0)
+            {
+                counter -= Time.deltaTime;
+            }
+            else
+            {
+                fadeIn = false;
+                counter = 2;
+            }
+        }
+	}
+
+    public void FadeIn()
+    {
+        fadeIn = true;
+    }
+
+    public void FadeOut()
+    {
+        fadeOut = true;
+    }
+
+    public bool Transparent { get { return transparent; } }
+}
